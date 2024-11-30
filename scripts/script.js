5b5d7a4875c484b5ef54626e358ff10a4b6e3fc0
Egor <egor@iMac-Egor.local>
2024-11-30 12:34:05 +0200

    test na telefon

--- scripts/script.js
+++ scripts/script.js
@@ -39,6 +39,36 @@ if (!selectedLanguage) {
     localStorage.setItem('selectedLanguage', selectedLanguage);
 }
 
+let contactContainer = document.querySelector('.contact-us-container');
+let contactButton = document.querySelector('.contact-us-button');
+let contactText = document.querySelector('.contact-text');
+let messengerIcons = document.querySelector('.messenger-icons');
+let contactButtonIcon = contactButton.querySelector('i');
+
+contactButton.addEventListener('click', ()=>{
+    //если иконки показываються, скрываем их
+    if (messengerIcons.classList.contains('show')){
+        messengerIcons.classList.remove('show');
+        contactText.classList.remove('hidden');
+        contactContainer.classList.remove('column-layout'); //возвращаем в row
+
+        contactButtonIcon.classList.remove('fa-solid', 'fa-rotate-left')
+        contactButtonIcon.classList.add('fa-regular', 'fa-message' );
+    }else{
+        contactText.classList.add('hidden');
+        contactContainer.classList.add('column-layout');
+
+        contactButtonIcon.classList.add('fa-solid', 'fa-rotate-left')
+        contactButtonIcon.classList.remove('fa-regular', 'fa-message' );
+
+        setTimeout(()=>{
+            messengerIcons.classList.add('show')
+        }, 10);
+    }
+
+    
+})
+
 const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
         //если элемент виден в данный момент
